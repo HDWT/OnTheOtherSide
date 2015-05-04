0306c9bd1f6a4f679a6f2747983a93f831fb3190
Alexander Rechkin <card.castles.in.the.sky@gmail.com>
2015-05-04 15:13:51 +0500

    First Commit

---
+++ Source/OnTheOtherSide/OnTheOtherSideGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "OnTheOtherSideGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONTHEOTHERSIDE_API AOnTheOtherSideGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
